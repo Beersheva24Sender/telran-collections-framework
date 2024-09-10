9f8be7ab28f0e1e5f42f169d8798e8b9fa674b81
Daniel Sender <70211437+daniel1sender@users.noreply.github.com>
2024-09-10 14:46:26 +0300

    make corrections in abstract map test

--- src/main/java/telran/util/AbstractMap.java
+++ src/main/java/telran/util/AbstractMap.java
@@ -1,5 +1,7 @@
 package telran.util;
 
+import java.util.Objects;
+
 @SuppressWarnings("unchecked")
 public abstract class AbstractMap<K, V> implements Map<K, V> {
     protected Set<Entry<K, V>> set;
@@ -9,8 +11,7 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
     @Override
     public V get(Object key) {
 
-        Entry<K, V> pattern = new Entry<>((K) key, null);
-        Entry<K, V> entry = set.get(pattern);
+        Entry<K, V> entry = getEntry(key);
         V res = null;
         if (entry != null) {
             res = entry.getValue();
@@ -18,49 +19,47 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
         return res;
     }
 
-    @Override
-    public V put(K key, V value) {
+    private Entry<K, V> getEntry(Object key) {
+        Entry<K, V> pattern = getPattern(key);
+        Entry<K, V> entry = set.get(pattern);
+        return entry;
+    }
 
-        Entry<K, V> pattern = new Entry<>((K) key, null);
-
-        Entry<K, V> existingEntry = set.get(pattern);
-        if (existingEntry != null) {
-            V oldValue = existingEntry.getValue();
-            existingEntry.setValue(value);
-            return oldValue;
-        }
-
-        set.add(new Entry<>(key, value));
-        return null;
+    private Entry<K, V> getPattern(Object key) {
+        return new Entry<>((K) key, null);
     }
 
     @Override
-    public boolean containsKey(Object key) {
-        Entry<K, V> pattern = new Entry<>((K) key, null);
+    public V put(K key, V value) {
 
-        Entry<K, V> existingEntry = set.get(pattern);
-        boolean res = existingEntry != null ? true : false;
+        Entry<K, V> entry = getEntry(key);
+        V res = null;
+        if (entry != null) {
+            res = entry.getValue();
+            entry.setValue(value);
+        } else {
+            set.add(new Entry<K, V>(key, value));
+        }
+
         return res;
     }
 
     @Override
+    public boolean containsKey(Object key) {
+        Entry<K, V> entry = getEntry(key);
+        return entry != null;
+    }
+
+    @Override
     public boolean containsValue(Object value) {
-        for (Entry<K, V> entry : set) {
-            if (entry.getValue() == null && value == null) {
-                return true;
-            }
-            if (entry.getValue() != null && entry.getValue().equals(value)) {
-                return true;
-            }
-        }
-        return false;
+        return set.stream().anyMatch(e -> Objects.equals(e.getValue(), value));
     }
 
     @Override
     public Set<K> keySet() {
         Set<K> keySet = getEmptyKeySet();
-        // TODO
-        return null;
+        set.stream().map(Entry::getKey).forEach(keySet::add);
+        return keySet;
     }
 
     @Override
@@ -70,11 +69,9 @@ public abstract class AbstractMap<K, V> implements Map<K, V> {
 
     @Override
     public Collection<V> values() {
-        List<V> valuesList = new ArrayList<>();
-        for (Entry<K, V> entry : set) {
-            valuesList.add(entry.getValue());
-        }
-        return valuesList;
+        ArrayList<V> collection = new ArrayList<>(set.size());
+        set.stream().map(Entry::getValue).forEach(collection::add);
+        return collection;
     }
 
     @Override
--- src/test/java/telran/util/HashMapTest.java
+++ src/test/java/telran/util/HashMapTest.java
@@ -11,7 +11,11 @@ public class HashMapTest extends AbstractMapTest {
 
     @Override
     <T> void runTest(T[] expected, T[] actual) {
-        // TODO
+        T[] expectedSorted = Arrays.copyOf(expected, expected.length);
+        Arrays.sort(expectedSorted);
+        T[] actualSorted = Arrays.copyOf(actual, actual.length);
+        Arrays.sort(actualSorted);
+        assertArrayEquals(expectedSorted, actualSorted);
 
     }
 
@@ -21,40 +25,4 @@ public class HashMapTest extends AbstractMapTest {
         map = new HashMap<>();
         super.setUp();
     }
-
-    @Test
-    void getTest() {
-        map.put(1, 1);
-        assertEquals(map.get(1), 1);
-        // testing an non existing entry
-        assertNull(map.get(2));
-    }
-
-    @Test
-    void putTest() {
-        assertNull(map.put(1, 1));
-        assertEquals(1, map.get(1));
-        assertEquals(1, map.size());
-
-        // Testing an existing key
-        assertEquals(1, map.put(1, 1));
-    }
-
-    @Test
-    void containsKeyTest() {
-        // adding a existing key
-        map.put(1, null);
-        assertTrue(map.containsKey(1));
-        // testing a not existing key
-        assertFalse(map.containsKey(2));
-    }
-
-    @Test
-    void containsValueTest() {
-        map.put(3, 3);
-        map.put(5, 3);
-        assertTrue(map.containsValue(3));
-        assertFalse(map.containsValue(4));
-    }
-
 }
